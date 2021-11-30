c49019ff066e80a7e2bca3727406efb0d85ba46f
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-11-30 16:05:35 +0100

    Tarea 4

--- pingpong.py
+++ pingpong.py
@@ -41,6 +41,11 @@ class PelotaPong:
             self.dir_y = -self.dir_y
         if self.y + self.alto >= VENTANA_VERT:
             self.dir_y = -self.dir_y
+    def reiniciar(self):
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+        self.dir_x = -self.dir_x
+        self.dir_y = random.choice([-5, 5])
 
 def main():
     # Inicialización de Pygame
