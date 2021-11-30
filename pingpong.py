0a83d1faceaec768eac86b82b8f4eb328941200f
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2021-11-30 17:55:13 +0100

    corregido error en def rebotar

--- pingpong.py
+++ pingpong.py
@@ -38,10 +38,12 @@ class PelotaPong:
         self.y += self.dir_y
 
     def rebotar(self):
-        if self.x <= 0:
-            self.dir_x = -self.dir_x
-        if self.x + self.ancho >= VENTANA_HORI:
-            self.dir_x = -self.dir_x
+        if self.x <= -self.ancho:
+            self.reiniciar()
+            self.puntuacion_ia += 1
+        if self.x >= VENTANA_HORI:
+            self.reiniciar()
+            self.puntuacion += 1
         if self.y <= 0:
             self.dir_y = -self.dir_y
         if self.y + self.alto >= VENTANA_VERT:
