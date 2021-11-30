fd548bcd4bcdfe89e431f56d4593e3f4326b64ff
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-11-30 15:11:22 +0100

    tarea 3

--- pingpong.py
+++ pingpong.py
@@ -32,6 +32,16 @@ class PelotaPong:
         self.x += self.dir_x
         self.y += self.dir_y
 
+    def rebotar(self):
+        if self.x <= 0:
+            self.dir_x = -self.dir_x
+        if self.x + self.ancho >= VENTANA_HORI:
+            self.dir_x = -self.dir_x
+        if self.y <= 0:
+            self.dir_y = -self.dir_y
+        if self.y + self.alto >= VENTANA_VERT:
+            self.dir_y = -self.dir_y
+
 def main():
     # Inicialización de Pygame
     pygame.init()
@@ -46,6 +56,7 @@ def main():
     jugando = True
     while jugando:
         pelota.mover()
+        pelota.rebotar()
 
         ventana.fill(BLANCO)
         ventana.blit(pelota.imagen, (pelota.x, pelota.y))
