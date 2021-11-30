8c901884b9aa41fbdb6d919f5540023a43738d11
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-11-30 15:05:46 +0100

    Tarea 2

--- pingpong.py
+++ pingpong.py
@@ -1,5 +1,6 @@
 # pong_1_1.py: Ventana de juego
 
+import random
 import pygame
 from pygame.locals import QUIT
 
@@ -9,6 +10,27 @@ VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
 
+class PelotaPong:
+    def __init__(self, fichero_imagen):
+        # --- Atributos de la Clase ---
+
+        # Imagen de la Pelota
+        self.imagen = pygame.image.load(fichero_imagen).convert_alpha()
+
+        # Dimensiones de la Pelota
+        self.ancho, self.alto = self.imagen.get_size()
+
+        # Posición de la Pelota
+        self.x = VENTANA_HORI / 2 - self.ancho / 2
+        self.y = VENTANA_VERT / 2 - self.alto / 2
+
+        # Dirección de movimiento de la Pelota
+        self.dir_x = random.choice([-5, 5])
+        self.dir_y = random.choice([-5, 5])
+
+    def mover(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
 
 def main():
     # Inicialización de Pygame
@@ -18,10 +40,15 @@ def main():
     ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
     pygame.display.set_caption("Pong 1")
 
+    pelota = PelotaPong("bola_roja.png")
+
     # Bucle principal
     jugando = True
     while jugando:
+        pelota.mover()
+
         ventana.fill(BLANCO)
+        ventana.blit(pelota.imagen, (pelota.x, pelota.y))
 
         for event in pygame.event.get():
             if event.type == QUIT:
